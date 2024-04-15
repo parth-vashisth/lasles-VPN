11655fe76f928ecba9c28c3cef3700b5f5f327b9
parth vashisth <shrmaparth123@gmail.com>
2024-04-15 16:18:36 +0530

    slick slider done

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,40 @@ let lgview = document.querySelector(".lg-view");
 navicon.addEventListener("click", () => {
     lgview.classList.toggle("show");
 });
+
+
+$(".parentslider").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    prevArrow: ".prev",
+    nextArrow: ".next",
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
